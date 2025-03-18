dcdaf3afc688a83f46217f56ed4f98445524a89d
Shafi327 <shafishangla327@gmail.com>
2025-03-18 12:53:58 +0500

    new changing

--- backend/server-php.js
+++ backend/server-php.js
@@ -1,47 +1,43 @@
-const express = require('express');
-const { 
-    LANG,
-    PORT, 
-    TEMPLATE_FILES_PATH
-} = require('./core/php/constants');
+const express = require("express");
+const { LANG, PORT, TEMPLATE_FILES_PATH } = require("./core/php/constants");
 
-const { 
-    matchPhpFile
-} = require('./core/php/match');
-
-
+const { matchPhpFile } = require("./core/php/match");
 
 const port = PORT;
 const app = express();
 
-
 // must specify options hash even if no options provided!
-const phpExpress = require('php-express')({
-
-    // assumes php is in your PATH
-    binPath: 'php'
+const phpExpress = require("php-express")({
+  // assumes php is in your PATH
+  binPath: "php",
 });
 
 // set view engine to php-express
-app.set('views', TEMPLATE_FILES_PATH);
-app.engine('php', phpExpress.engine);
-app.set('view engine', 'php');
+app.set("views", TEMPLATE_FILES_PATH);
+app.engine("php", phpExpress.engine);
+app.set("view engine", "php");
 
 // CORS on ExpressJS
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Content-Type", "application/json");
-    next();
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Content-Type", "application/json");
+  next();
 });
 
 // routing all .php file to php-express
 app.all(matchPhpFile, phpExpress.router);
 
+app.get("/", (req, res) => {
+  res.send("PHP server listening at http://localhost:4000");
+});
 
-require('./plugins/signal');
-const server = app.listen(port, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(LANG.en.serverRun, host, port);
+require("./plugins/signal");
+const server = app.listen(4000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("PHP server listening at http://localhost:4000");
 });
--- next.config.js
+++ next.config.js
@@ -1,67 +1,65 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 // Docker deployment
-// To add support for Docker to an existing project, 
+// To add support for Docker to an existing project,
 // you can directly set the `dockerDeploymentEnabled` property to `true`
 const dockerDeploymentEnabled = false;
 
 // Static Exports
-let exportHtmlEnabled = process.env.EXPORT_ENABLED == 'false' ? false : true;
+let exportHtmlEnabled = process.env.EXPORT_ENABLED == "false" ? false : true;
 if (dockerDeploymentEnabled) exportHtmlEnabled = false;
-
 
 //
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    eslint : {
-        ignoreDuringBuilds: true,
-    },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
-
-    // Use the "https://yourusername.github.io/my-subdirectory/ " in production and localhost for development.
-    // !!!IMPORTANT: You need to modify the relevant paths in the "scripts/config-rootdir-of-publishing-source.js" at the same time
-    /*
+  // Use the "https://yourusername.github.io/my-subdirectory/ " in production and localhost for development.
+  // !!!IMPORTANT: You need to modify the relevant paths in the "scripts/config-rootdir-of-publishing-source.js" at the same time
+  /*
     basePath: isProd ? '/my-subdirectory' : undefined,
     assetPrefix: isProd ? '/my-subdirectory/' : undefined,
     */
 
-    // disable source map 
-    productionBrowserSourceMaps: true,
+  // disable source map
+  productionBrowserSourceMaps: true,
 
-    // !!! for docker (`output: 'standalone'`)
-    // This will create a folder at .next/standalone which can then be deployed on its own without installing node_modules.
+  // !!! for docker (`output: 'standalone'`)
+  // This will create a folder at .next/standalone which can then be deployed on its own without installing node_modules.
 
-    output: dockerDeploymentEnabled ? 'standalone' : !exportHtmlEnabled ? undefined : 'export',
+  output: dockerDeploymentEnabled
+    ? "standalone"
+    : !exportHtmlEnabled
+    ? undefined
+    : "export",
 
+  // image optimize
+  images: {
+    unoptimized: true,
+  },
 
-    // image optimize
-    images: {
-        unoptimized: true
-    },
+  //  add a page route with html extension
+  // Rename the file under pages directory to `.html.tsx`
+  pageExtensions: ["html.jsx", "jsx", "js", "tsx", "ts"],
+  // omit the html extension
+  trailingSlash: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-
-    //  add a page route with html extension 
-    // Rename the file under pages directory to `.html.tsx`
-    pageExtensions: ['html.jsx', 'jsx', 'js', 'tsx', 'ts'],
-    // omit the html extension 
-    trailingSlash: false,
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
-
-
-        return config;
-    },
-    env: {
-        exportHtml: `${exportHtmlEnabled}`
-    }
-    /*
+    return config;
+  },
+  env: {
+    exportHtml: `${exportHtmlEnabled}`,
+  },
+  /*
 
     // change the api route
     async rewrites() {
@@ -100,8 +98,6 @@ const nextConfig = {
         ]
     },
     */
-}
+};
 
-module.exports = nextConfig
-
-
+module.exports = nextConfig;
